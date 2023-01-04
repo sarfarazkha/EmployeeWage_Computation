0cdea3dd0b7216f60b4f801f6c9061093251a776
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-04 22:34:32 +0530

    Added: Part Time Employee & Wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,16 +1,23 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
-    static final int IS_FULL_TIME = 8;
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
     static final int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String[] args) {
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
+
             empHrs = 8;
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage:" + empWage);
+        }
+        else if (empCheck == IS_PART_TIME){
+                empHrs = 4;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Employee Wage:" + empWage);
+        }
     }
 }
