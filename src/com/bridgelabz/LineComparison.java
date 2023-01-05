e651afccf59e9ba3c65d4c2a7c8d75aacb09e71d
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-05 22:53:29 +0530

    Added : ompairing two lines are equal or not using equals method …

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,19 +1,37 @@
 package com.bridgelabz;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line comparison Program");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter co-ordinates of first point :");
+
+        System.out.println("Enter co-ordinates of first point of First line :");
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
-        System.out.println("Enter co-ordinates of second point :");
+        System.out.println("Enter co-ordinates of second point of First line :");
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
-        float length;
-        length = (float) Math.sqrt((Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2)));
-        System.out.println("Length of line is : " + length);
+
+        System.out.println("Enter co-ordinates of first point of Second line :");
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        System.out.println("Enter co-ordinates of second point of second line :");
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+
+        float length1, length2;
+        length1 = (float) Math.sqrt((Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2)));
+        length2 = (float) Math.sqrt((Math.pow((a1 - a2), 2)) + (Math.pow((b1 - b2), 2)));
+        System.out.println("Length of lines are : " + length1 + " & " + length2);
+
+        if (Objects.equals(length1, length2)) {
+            System.out.println("Lines are of Equal length");
+        } else {
+            System.out.println("Lines are of different length");
+        }
     }
 }
