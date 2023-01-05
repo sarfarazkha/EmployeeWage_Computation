d64adae70173207893b88db3462314037dd34651
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-05 22:31:35 +0530

    Added : Condition of total working hours or days

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,31 +1,42 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
-    public static final int IS_ABSENT = 0;
-    public static final int WAGE_PER_HOUR = 20;
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
+    public static final int FULL_TIME = 8;
+    public static final int PART_TIME = 4;
+    public static final int TOTAL_WORKING_DAYS = 20;
+    public static final int TOTAL_WORKING_HRS = 100;
 
     public static void main(String[] args) {
-        int wrkHrs = 0;
-        int empCheck=(int)(Math.random()*10)%3;
+        //int empHrs = 0;
+        int workdays = 1;
+        int totalwage = 0;
 
-        switch(empCheck) {
-            case IS_FULL_TIME : wrkHrs=16;
-                System.out.println("Employee is Present Full Time and working hours is = "+wrkHrs);
-                break;
-
-            case IS_PART_TIME : wrkHrs=8;
-                System.out.println("Employee is Part Time and working hours is = "+wrkHrs);
-                break;
-
-            case IS_ABSENT : wrkHrs=0;
-                System.out.println("Employee is Absent and working hours = "+wrkHrs);
-                break;
-
-            default:System.out.println("Invalid Input");
+        int workingHrs = 0;
+        while (workdays <= TOTAL_WORKING_DAYS && workingHrs <= TOTAL_WORKING_HRS) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            int empWage = 0;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Full time Present");
+                    empWage = FULL_TIME * EMPLOYEE_WAGE_PER_HOUR;
+                    workingHrs+=FULL_TIME;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Part time Present");
+                    empWage = PART_TIME * EMPLOYEE_WAGE_PER_HOUR;
+                    workingHrs+=PART_TIME;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            totalwage+=empWage;
+            System.out.println("Employee wage for Day #" + workdays + " =>  " + empWage);
+            workdays++;
         }
-        System.out.println("Employee's Daily Wage is = "+(wrkHrs*WAGE_PER_HOUR));
-
+        System.out.println("Total wage: "+totalwage);
+        System.out.println("Totalworking Hrs : "+workingHrs);
     }
 }
