7e8480b96b515df77bd9bff8e1f065cf1057b02a
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-06 23:04:53 +0530

    Added: Using compare checking equal, greater or less

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -6,32 +6,34 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line comparison Program");
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
 
         System.out.println("Enter co-ordinates of first point of First line :");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
+        int x1 =sc.nextInt();
+        int y1= sc.nextInt();
         System.out.println("Enter co-ordinates of second point of First line :");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
+        int x2 =sc.nextInt();
+        int y2= sc.nextInt();
 
         System.out.println("Enter co-ordinates of first point of Second line :");
-        int a1 = sc.nextInt();
-        int b1 = sc.nextInt();
+        int a1 =sc.nextInt();
+        int b1= sc.nextInt();
         System.out.println("Enter co-ordinates of second point of second line :");
-        int a2 = sc.nextInt();
-        int b2 = sc.nextInt();
+        int a2 =sc.nextInt();
+        int b2= sc.nextInt();
 
 
-        float length1, length2;
-        length1 = (float) Math.sqrt((Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2)));
-        length2 = (float) Math.sqrt((Math.pow((a1 - a2), 2)) + (Math.pow((b1 - b2), 2)));
-        System.out.println("Length of lines are : " + length1 + " & " + length2);
+        float length1,length2;
+        length1 = (float) Math.sqrt((Math.pow((x1-x2),2))+(Math.pow((y1-y2),2)));
+        length2 = (float) Math.sqrt((Math.pow((a1-a2),2))+(Math.pow((b1-b2),2)));
+        System.out.println("Length of lines are : "+length1+" & "+length2);
 
-        if (Objects.equals(length1, length2)) {
-            System.out.println("Lines are of Equal length");
-        } else {
-            System.out.println("Lines are of different length");
+        if (Float.compare(length1, length2) >0) {
+            System.out.println("Line 1 is greater than line 2");}
+        else if (Float.compare(length1, length2) < 0) {
+            System.out.println("Line 2 is greater than line 1");}
+        else{
+            System.out.println("Both lines are Equal");
         }
     }
 }
